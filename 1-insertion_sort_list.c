625dcdf158e5f5f76f386499b9bf4d5b24e6ca8b
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-09 15:07:35 +0200

    Insert list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,34 +9,38 @@
  */
 listint_t * SortList(listint_t * pList)
 {
-  // zero or one element in list
-  if (!pList || !pList->next)
-      return pList;
 
-  /* build up the sorted array from the empty list */
-  listint_t * pSorted = NULL;
-
-  /* take items off the input list one by one until empty */
-  while (pList != NULL) {
-      /* remember the head */
-      listint_t *  pHead  = pList;
-      /* trailing pointer for efficient splice */
-       listint_t ** ppTrail = &pSorted;
-
-      /* pop head off list */
-      pList = pList->next;
-
-      /* splice head into sorted list at proper place */
-      while (!(*ppTrail == NULL || pHead->n < (*ppTrail)->n)) { /* does head belong here? */
-          /* no - continue down the list */
-          ppTrail = &(*ppTrail)->next;
-      }
-
-      pHead->next = *ppTrail;
-      *ppTrail = pHead;
-  }
-
-  return pSorted;
+	// zero or one element in list
+    if (pList == NULL || pList->next == NULL)
+        return pList;
+    // head is the first element of resulting sorted list
+    listint_t *head = NULL;
+    while (pList != NULL) {
+        print_list(pList);
+	listint_t * current = pList;
+        pList = pList->next;
+        if (head == NULL || current->n < head->n) {
+            // insert into the head of the sorted list
+            // or as the first element into an empty sorted list
+            current->next = head;
+            head = current;
+        } else {
+            // insert current element into proper position in non-empty sorted list
+            listint_t *p = head;
+            while (p != NULL) {
+                if (p->next == NULL || // last element of the sorted list
+                    current->n < p->next->n) // middle of the list
+                {
+                    // insert into middle of the sorted list or as the last element
+                    current->next = p->next;
+                    p->next = current;
+                    break; // done
+                }
+                p = p->next;
+            }
+        }
+    }
+    return head;
 }
 /** 
  * insertion_sort_list - tri in insert mode
