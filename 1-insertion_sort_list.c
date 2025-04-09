4a9e58a428962f98e471e243e659209006a4811e
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-09 15:17:36 +0200

    insert sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-#include <stddef.h> 
+#include <stddef.h>
 
 /**
  * SortList
@@ -7,40 +7,39 @@
  *
  * Return: List sort
  */
-listint_t * SortList(listint_t * pList)
+listint_t *SortList(listint_t *pList)
 {
+	listint_t *current;
+	listint_t *head = NULL;
 
-	// zero or one element in list
-    if (pList == NULL || pList->next == NULL)
-        return pList;
-    // head is the first element of resulting sorted list
-    listint_t *head = NULL;
-    while (pList != NULL) {
-        print_list(pList);
-	listint_t * current = pList;
-        pList = pList->next;
-        if (head == NULL || current->n < head->n) {
-            // insert into the head of the sorted list
-            // or as the first element into an empty sorted list
-            current->next = head;
-            head = current;
-        } else {
-            // insert current element into proper position in non-empty sorted list
-            listint_t *p = head;
-            while (p != NULL) {
-                if (p->next == NULL || // last element of the sorted list
-                    current->n < p->next->n) // middle of the list
-                {
-                    // insert into middle of the sorted list or as the last element
-                    current->next = p->next;
-                    p->next = current;
-                    break; // done
-                }
-                p = p->next;
-            }
-        }
-    }
-    return head;
+	if (pList == NULL || pList->next == NULL)
+		return (pList);
+	while (pList != NULL) 
+	{
+		print_list(head);
+		current = pList;
+		pList = pList->next;
+		if (head == NULL || current->n < head->n)
+		{
+			current->next = head;
+			head = current;
+		} 
+		else 
+		{
+			listint_t *p = head;
+			while (p != NULL) {
+				if (p->next == NULL || 
+						current->n < p->next->n) 
+				{
+					current->next = p->next;
+					p->next = current;
+					break; 
+				}
+				p = p->next;
+			}
+		}
+	}
+	return head;
 }
 /** 
  * insertion_sort_list - tri in insert mode
